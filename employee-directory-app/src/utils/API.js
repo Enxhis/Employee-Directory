2565e72df5b838faa844a4b241b0babf35f67501
Enxhi Salaj <enxhi_salaj@yahoo.com>
2021-02-04 18:53:11 -0500

    fixed API

--- employee-directory-app/src/utils/API.js
+++ employee-directory-app/src/utils/API.js
@@ -1,11 +1,5 @@
-import axios from 'axios';
+import axios from "axios";
 
-// baseurl from Random API
-const BASEURL = "https://randomuser.me/api/";
-
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {
-    GetEmployees: () => {
-        return axios.get(BASEURL);
-    }
+export const getEmployees = function() {
+  return axios.get("https://randomuser.me/api/?results=200&nat=us");
 };
