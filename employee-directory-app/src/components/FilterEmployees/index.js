8d97f75184747bad9a81e3d2398d2e36335df711
Enxhi Salaj <enxhi_salaj@yahoo.com>
2021-02-04 18:57:14 -0500

    added components and their styling files

---
+++ employee-directory-app/src/components/FilterEmployees/index.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import "./style.css";
+
+
+const FilterEmployees = ({ employees, updateEmployees }) => {
+    const [inputValue, updateInput] = useState("");
+
+    useEffect(() => {
+        const filteredEmployees =
+            inputValue === "" ? employees : employees.filter(
+                ({ name: { first } }) =>
+                    first.toLowerCase().indexOf(inputValue.toLowerCase()) >= 0
+            );
+
+        updateEmployees(filteredEmployees);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [inputValue, employees]);
+
+    return (
+        <div className="filter-data">
+            <p className='sub-header'>
+                To filter by first name, please begin your search in the user input
+                below!
+            </p>
+            <input className='input-box' value={inputValue} onChange={e => updateInput(e.target.value)} />
+        </div>
+    );
+};
+
+export default FilterEmployees;
